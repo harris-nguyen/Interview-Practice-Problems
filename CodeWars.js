1374494a8de128303b8503f8462c0ac57babac1c
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-07-05 17:59:31 -0700

    updated with new problems

--- CodeWars.js
+++ CodeWars.js
@@ -9,3 +9,9 @@ function arrayDiff(a, b) {
 }
 console.log(arrayDiff([1, 2], [1])); //[2]
 console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
+//
+function highAndLow(num) {
+  let number = num.split(' ').sort((a, b) => {a - b})
+  return `${number[number.length -1]} ${number[0]}`
+}
+console.log(highAndLow("1 2 3 4 5")) // return "5 1"
