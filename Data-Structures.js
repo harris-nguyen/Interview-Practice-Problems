740e909e6f44af3c0d22071baec82bfee1469421
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-07-13 11:58:22 -0700

    update

--- Data-Structures.js
+++ Data-Structures.js
@@ -16,3 +16,18 @@ var containsDuplicate = function (nums) {};
 
 var rotate = function (nums, k) {};
 // https://leetcode.com/problems/rotate-array/description/
+
+// O(n) and faster
+function firstRecurringCharacter(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+    console.log(map);
+  }
+  return undefined;
+}
+console.log(firstRecurringCharacter([2, 5, 9, 8, 2, 4, 5, 7, 9]));
