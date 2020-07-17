e2eb4b6b416364d7683227dc262758750e4de20a
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-07-17 11:55:41 -0700

    update

--- Data-Structures/Binary-Search-Tree.js
+++ Data-Structures/Binary-Search-Tree.js
@@ -2,6 +2,13 @@
 
 // balanced binary search tree
 // lookup, insert, & delete = O(log N)
+// you normally won't be asked to talk about delete in interviews
+
+// AVL Tree
+  //https://medium.com/basecs/the-little-avl-tree-that-could-86a3cae410c7
+
+// Red Black Tree
+  // https://medium.com/basecs/painting-nodes-black-with-red-black-trees-60eacb2be9a5
 
 class Node {
   constructor(value) {
