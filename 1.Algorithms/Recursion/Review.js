cd7d740aaca646722bec16cf5d64c45a20b7f202
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-07-19 13:58:52 -0700

    update

---
+++ 1.Algorithms/Recursion/Review.js
@@ -0,0 +1,16 @@
+
+// Recursion and Space Complexity are not friends
+
+// Pros
+  // readable
+  // When creating / working with trees
+  // Good for:
+    // Merge Sort
+    // Quick Sort
+    // Tree Traversal
+    // Graph Traversal
+
+// Cons
+  // can take up a lot of space/time
+  // stack overflow
+  // looping can still be a better choice
