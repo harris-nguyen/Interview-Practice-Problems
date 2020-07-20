1f0085ab92319f117e9a390267dd3d6357e2f3fc
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-07-20 14:38:59 -0700

    update

--- 1.Algorithms/Sorting/Selection Sort.js
+++ 1.Algorithms/Sorting/Selection Sort.js
@@ -10,15 +10,16 @@ function selectionSort(array) {
     let temp = array[i]; // [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
     for (let j = i + 1; j < length; j++) {
+      // starting at index 1
+      // if 44 < 99
       if (array[j] < array[min]) {
-        // if 44 < 99
         //update minimum if current is lower that what we had previously
-        min = j; // index 1
+        min = j; // min = index 1
       }
     }
 
-    // array[i] // 99
-    array[i] = array[min]; // 44, 99
+    // array[i] = 99     array[min] = 44
+    array[i] = array[min]; // 44
     array[min] = temp; // 99
   }
   return array;
