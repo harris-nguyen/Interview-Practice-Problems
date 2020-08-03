e69af9199fc39914d6b6fd76eefe5af68ceaab55
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-08-03 12:08:27 -0700

    update

--- Exercise/chunk/index.js
+++ Exercise/chunk/index.js
@@ -8,6 +8,12 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
-
-module.exports = chunk;
+function chunk(array, size) {
+  const chunked_arr = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked_arr.push(array.slice(index, size + index));
+    index += size;
+  }
+  return chunked_arr;
+}
