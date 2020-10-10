4d2478da978c70a3ae27951b88006292f4073d99
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-10-10 13:02:53 -0700

    never give up

--- Exercise/anagrams/index.js
+++ Exercise/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const stringA1 = stringA
+      .replace(/[^\w]/g, "")
+      .toLowerCase()
+      .split("")
+      .sort()
+      .join("");
+    const stringB1 = stringB
+      .replace(/[^\w]/g, "")
+      .toLowerCase()
+      .split("")
+      .sort()
+      .join("");
+
+    for (let i = 0; i < stringA1.length; i++) {
+      if (stringA1.length === stringB1.length) {
+        if (stringA1[i] === stringB1[i]) {
+          console.log(true);
+          return true;
+        } else {
+          console.log(false);
+          return false;
+        }
+      } else {
+        console.log(false);
+      }
+    }
+}
 
 module.exports = anagrams;
