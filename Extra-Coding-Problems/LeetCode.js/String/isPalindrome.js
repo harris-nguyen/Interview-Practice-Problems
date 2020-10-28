2f1d1860434241eb1c2efca9739304c1606010aa
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-10-28 12:50:38 -0700

    updating

---
+++ Extra-Coding-Problems/LeetCode.js/String/isPalindrome.js
@@ -0,0 +1,11 @@
+//hint: use replace method
+var isPalindrome = function (str) {
+  const expr = /[\W_]/g;
+
+
+};
+//
+// isPalindrome("a man a plan a canal panama"); // true
+// isPalindrome("1890981"); // true
+// isPalindrome("race a car"); // false
+// Output: true;
