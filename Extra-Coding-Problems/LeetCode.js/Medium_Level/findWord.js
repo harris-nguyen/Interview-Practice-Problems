8a1a029ba2b562991fc379242707232633a8bf22
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-11-27 13:37:26 -0800

    updating

--- Extra-Coding-Problems/LeetCode.js/Medium_Level/findWord.js
+++ Extra-Coding-Problems/LeetCode.js/Medium_Level/findWord.js
@@ -7,12 +7,12 @@ function findWord(words, string){
 }
 
 
-const words = ["cat", "baby", "dog", "bird", "car", "ax"];
-const string1 = "tcabnihjs";
-const string2 = "tbcanihjs";
-const string3 = "baykkjl";
-const string4 = "bbabylkkj";
-const string5 = "ccc";
-const string6 = "nbird";
+// const words = ["cat", "baby", "dog", "bird", "car", "ax"];
+// const string1 = "tcabnihjs";
+// const string2 = "tbcanihjs";
+// const string3 = "baykkjl";
+// const string4 = "bbabylkkj";
+// const string5 = "ccc";
+// const string6 = "nbird";
 
 findWord(words, string1);
