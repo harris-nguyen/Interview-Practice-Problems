ebcc83043ff6cf9431bd5f6a261aca22f4cee9b2
Harris Nguyen <harrisq.nguyen@gmail.com>
2020-12-06 14:20:05 -0600

    updating

--- Extra-Coding-Problems/LeetCode.js/Array/climbStairsDP.js
+++ Extra-Coding-Problems/LeetCode.js/Array/climbStairsDP.js
@@ -18,3 +18,16 @@ function climbStairsDP(n) {
   console.log(arr[n]);
 }
 climbStairsDP(3);
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
