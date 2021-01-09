eed94a00c2093c6d5a8859def8eed4873dbf1090
Harris Nguyen <harrisq.nguyen@gmail.com>
2021-01-09 13:26:25 -0800

    updating

---
+++ Java/collection_Interface.js
@@ -0,0 +1,35 @@
+// collection sits on top of the sub interfaces and implementing classes.Seta collection can hoold a group of objects
+// The collection is extended by:
+// - set
+// - list
+// - queue
+
+// A set is defined as a grouop of unique objects. What is ocnsidred as unique is defined
+// by the equals method of the Object typeof. A sen can NOT hold two equal objects. No duplicates
+
+// A List can contain duplicates. A list keeps its elements in the order they were inputed
+
+// A Queue. Enter from the end and taken out from the top. FIRST IN FIRST OUT
+
+// SET
+// - HashSet -  normally the default for set
+// - LinkedHashSet - mix of hash and list. No duplicates. returns order in which they were inserted
+// - TreeSet - list based on sorted order.
+// - interface SortedSet - set is always sorted
+// - interface NavigableSet - provides methods to retrieve the next element
+
+// LIST
+// - ArrayList - it does allow duplicates and allows to iterate the list in the order of insertion.
+//   Fast to iterate and read BUT add or removing will be slow as this will require to rebuild the whole array
+// - LinkedList - makes it easy to add or remove elements at any position
+
+// QUEUE
+// - LinkedList - default for Queue. makes it easy to add or remove elements at any position
+// - PriorityQueue - Keeps its elements automatically ordered. similar to treeSet but it alows duplicates
+
+// MAP interface
+// - Hashmap - default for MAP. Does not make any guarantees on how it internally stores its elements
+// - LinkedHashMap - allows to oiterate the map in the oorder of inseration
+// - SortedMap - sorted map
+// - NavigableMap - adds methods to negigate thrugh the map. Allos you to retrieve all entries bigger or smaller for example
+// - TreeMap -
---
+++ Job_Interview_Q/FetchRewards/questions.js
@@ -0,0 +1,33 @@
+// Frontend Engineer:
+
+// What data will you cache
+// What cache storage will you use
+// Do you have any experience with Analytics on page
+// What tools did you use in your previous company
+// Backend Software Engineer:
+
+// Experience with REST/APIs
+// High-level architecture
+// Automated testing
+// Deployment
+// Able to explain complex concepts / topics in detail
+// Be able to speak to program languages on your resume. If you add languages you are not an expert in - remove them
+// Android:
+
+// Know how to handle the delay response
+// AWS technologies
+// Familiar with MVVM Architecture
+// Theoretical knowledge experience
+// iOS:
+
+// Good understanding on how to design the endpoints properly while taking into consideration of scaling and using query params
+// Experience doing App Store deployments
+// MVVM experience
+// Capable of being able to contribute to the code base
+// Data Scientist:
+
+// Knowledge of Spark
+// Build data pipelines
+// Coachable, personable, kind, easy to connect with, etc.
+// Machine learning experience is great to have
+// Consumer products experience nice to have
