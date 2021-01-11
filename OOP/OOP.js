c06a6a5386053585638b4bcf8d63b3c2bd855046
Harris Nguyen <harrisq.nguyen@gmail.com>
2021-01-10 19:07:36 -0800

    updating

--- OOP/OOP.js
+++ OOP/OOP.js
@@ -1,12 +1,56 @@
+// Object Oriented are based on the concept of objects
+
 // Object Oriented
-// - Encapsulation
 // - Abstraction
-// - Inheritance
 // - Polymorph
+// - Encapsulation
+// - Inheritance
 
 //!! Each class represents an object which has its own methods //!!
 
 // https://www.youtube.com/watch?v=1ONhXmQuWP8
+
+// Object Oriented
+
+// - Abstraction
+//     - Exposing only what is needed for the user to see. For example: Imagine a DVD player. You have buttons you can see but all the methods
+//       are behind the scenes
+//     - Simpler interface
+//     - Reduces the impact of change
+//         - When changes are made, it will not affect anything outside of scope
+//     - Only show the neccessary detail to the user (buttons)
+//         - Only care about calling the method but not the underline how things work inside
+
+// - Inheritance
+//     - Gives you the power to reuse and can add to it
+//     - Allows you to eliminate redundant code
+//     - Inherit other objects, methods
+//     -! classes that are derived from an existing class are called:
+          // SUBCLASS
+          // EXTENDED class
+          // CHILD CLASS
+        // These can be used by using extends
+       // ^ where these sub classes came from are known as:
+          // SUPER CLASS
+          // PARENT class
+          // BASE class
+
+// - Polymorph (many forms)
+//     - Git rid of long if and else or switch statements
+//     - Determines what should run while the program is running
+//     - Determines what function should run while the program is running
+//       - figured out during runtime. looks at what object it is pointed to then
+//       - looks at the method and if it's overwritten, it'll call the child object
+
+//  - Encapsulation (the action of enclosing something in or as if in a capsule)
+//    - Built on the idea of data hiding
+  //  - Same idea as scope?
+  //  - Able to restrict access to whatever is calling that object
+  //  - A group of relatable variables and functions into an unit/object
+  //  - We refer to the variables as properties
+  //  - We refer functions as methods
+  //  - Encapsulating our properties within the object
+// ￼
 
 // Procedural Programming
 // - Divides a program into different functions
@@ -18,28 +62,3 @@
 // - Abstraction: Reduce complexity + isolate impact of changes
 // - Inheritance: Eliminate redundant code
 // - Encapsulation: Refactor ugly switch/case statements
-
-// Object Oriented
-// - Encapsulation:
-// - Data hiding?
-// - Same idea as scope?
-// - Able to restrict access to whatever is calling that object
-// - A group of relatable variables and functions into an unit/object
-// - We refer to the variables as properties
-// - We refer functions as methods
-// ￼
-
-// - Abstraction
-//     - Imagine a DVD player. You have buttons you can see but all the methods or behind the scene work is left unseen
-//     - Simpler interface
-//     - Reduces the impact of change
-//         - When changes are made, it will not affect anything outside of scope
-//     - Only show the neccessary detail to the user (buttons)
-//         - Only care about calling the method but not the underline how things work inside
-// - Inheritance
-//     - Allows you to eliminate redundant code
-//     - Inherit other objects, methods
-//     - Gives you the power to reuse and can add to it
-// - Polymorph (many forms)
-//     - Git rid of long if and else or switch statements
-//     - Determines what should run while the program is running
